40cc75a72daf256c754fb55ace72db83ace2eaaf
SnazzyTrack9218 <72895116+SnazzyTrack9218@users.noreply.github.com>
2025-05-23 18:28:49 -0400

    Update DiscordBotComp.py

--- DiscordBotComp.py
+++ DiscordBotComp.py
@@ -32,7 +32,7 @@ DEFAULT_CONFIG = {
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 # Data storage
 applications = {}
